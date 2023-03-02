bccbc05071565ff357657c0e48ab19a98869e846
drex007 <vykeoj@gmail.com>
2023-03-02 08:48:06 +0000

    Initial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"> 
+      </header>
+      <h1 className="text-4xl text-center">HI, This is react</h1>
+    </div>
+  );
+}
+
+export default App;
